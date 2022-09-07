95512d3ac41b771ec0b3cea1eaa9542b0d68c54f
Sergi Tubert <stubertsabater@gmail.com>
2022-09-07 20:29:25 +0200

    First commit

---
+++ src/main/java/com/ironhack/sergitubertironbank/SergitubertIronbankApplication.java
@@ -0,0 +1,13 @@
+package com.ironhack.sergitubertironbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SergitubertIronbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SergitubertIronbankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ironhack/sergitubertironbank/SergitubertIronbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ironhack.sergitubertironbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SergitubertIronbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
