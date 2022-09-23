1e73eadb568dd57f511636a3f13e61e4b662eb94
Sergi Tubert <stubertsabater@gmail.com>
2022-09-23 17:59:34 +0200

    not nough balance exception resposne status

--- src/main/java/com/ironhack/sergitubertironbank/accounts/shared/domain/NotEnoughBalanceException.java
+++ src/main/java/com/ironhack/sergitubertironbank/accounts/shared/domain/NotEnoughBalanceException.java
@@ -1,5 +1,9 @@
 package com.ironhack.sergitubertironbank.accounts.shared.domain;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class NotEnoughBalanceException extends Exception {
     public NotEnoughBalanceException(String iban) {
         super(String.format("Account with IBAN: %s don't have enough balance", iban));
